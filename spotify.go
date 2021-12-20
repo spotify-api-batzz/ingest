9980c7bac7e5d7641b2cf04db1cdc3b7bca2beb0
Batu Ellery <batublob@gmail.com>
2021-12-20 21:27:29 +0000

    printing artist ids

--- spotify.go
+++ spotify.go
@@ -552,7 +552,7 @@ Outer:
 	for id := range artistSpotifyIDs.UniqueMap {
 		for _, dbArtist := range dbArtists {
 			if dbArtist.SpotifyID == id {
-				logger.Log(fmt.Sprintf("Database already contains artist %s (%s)", dbArtist.Name, dbArtist.SpotifyID), logger.Debug)
+				logger.Log(fmt.Sprintf("Database already contains artist %s (%s)", dbArtist.Name, dbArtist.SpotifyID), logger.Trace)
 				continue Outer
 			}
 		}
