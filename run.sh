28eee909ba972245213c5548d16d92a53e5faa6e
Batu Ellery <batublob@gmail.com>
2021-12-20 19:16:07 +0000

    added tom to rung

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/Desktop/projects/spotify/spotify
-for var in "bungusbuster" "anneteresa-gb"
+for var in "bungusbuster" "anneteresa-gb" "tomadams1997"
 do 
 	./spotify --u $var --a --r --t
 done
